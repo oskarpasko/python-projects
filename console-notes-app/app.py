75d16d39b3ffa6a953ab31446eb0adff4e4f5588
Oskar <>
2023-04-24 11:59:31 +0200

    add languages to project

--- console-notes-app/app.py
+++ console-notes-app/app.py
@@ -1,3 +1,21 @@
 from src.menu import *
 
-menu_pl()
+print(f"{Colors.YELLOW}++++++++++ Console Notes App ++++++++++{Colors.END}")
+
+while (True):
+    menu(LANGUAGE)
+
+    choice = input("->  ")
+
+    match(choice):
+        case '4':
+            print(f"{Colors.BLUE}1. Polski")
+            print(f"2. English{Colors.END}")
+            lang_choice = input("-> ")
+            match(lang_choice):
+                case '1':
+                    LANGUAGE = 'pl'
+                case '2':
+                    LANGUAGE = 'eng'
+                case _:
+                    print(f"{Colors.RED}Error 12!{Colors.END}")
