70e739d6733f1f12c68d929820854da933173a28
Oskar <>
2023-04-24 11:48:22 +0200

    add first menu to project

---
+++ console-notes-app/app.py
@@ -0,0 +1,3 @@
+from src.menu import *
+
+menu_pl()
---
+++ console-notes-app/src/menu.py
@@ -0,0 +1,9 @@
+from .settings import *
+
+def menu_pl():
+    print(f"{Colors.LIGHT_BLUE}1. Nowa notatka")
+    print("2. Czytaj notatki")
+    print("3. Usuń notatkę")
+    print("4. Język")
+    print(f"5. Wyjdź{Colors.END}")
+    
