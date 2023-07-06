c8e5564b2ba64413858deea38122cbf6e1ccb5ab
Oskar <oskarpasko@gmail.com>
2023-07-06 14:04:06 +0200

    add some styles to the console

--- mongo-user-managment/app.py
+++ mongo-user-managment/app.py
@@ -1,22 +1,24 @@
 from pymongo import MongoClient
 from getpass import getpass
+from settings import *
 
 client = MongoClient("mongodb://localhost:27017")
 db = client.loginData
 users = db.user
 
 while True:
-    print("\n ---------- Login Page ---------- \n")
-    print('If you wan to exit write "exit" in login.')
-    login = input("Insert Your login: ")
-    if login == 'exit': exit("Bye, have a great time!")
-    password = getpass("Insert Your password: ")
+    print(f"{Colors.YELLOW}\n ---------- Login Page ---------- \n{Colors.END}")
+    print(f'{Colors.GREEN}If you wan to exit write "exit" in login.{Colors.END}')
+    login = input(f"{Colors.BLUE}Insert Your login: {Colors.END}")
+    if login == 'exit': exit(f"{Colors.PURPLE}Bye, have a great time!{Colors.END}")
+    password = getpass(f"{Colors.BLUE}Insert Your password: {Colors.END}")
 
     temp = users.find_one({'login': f'{login}', 'password': f'{password}'})
 
     if temp == None:
-        print("Wrong login or password!")
+        print(f"{Colors.RED}\nWrong login or password!{Colors.END}")
     else:
-        user = users.find_one({'login': f'{login}', 'password': f'{password}'})
-        details = user['details']
-        print(f"Welcome back {details['fname']}!")
+        break
+user = users.find_one({'login': f'{login}', 'password': f'{password}'})
+details = user['details']
+print(f"{Colors.CYAN}\nWelcome back {details['fname']}!{Colors.END}")
