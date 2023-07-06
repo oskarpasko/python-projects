83781f0885ce8a2afc3e91c35f9cf855c6c7b899
Oskar <oskarpasko@gmail.com>
2023-07-06 13:40:17 +0200

    add loop and mechanism to exit from this loop

--- mongo-user-managment/app.py
+++ mongo-user-managment/app.py
@@ -5,15 +5,18 @@ client = MongoClient("mongodb://localhost:27017")
 db = client.loginData
 users = db.user
 
-print(" ---------- Login Page ---------- \n")
-login = input("Insert Your login: ")
-password = getpass("Insert Your password: ")
+while True:
+    print("\n ---------- Login Page ---------- \n")
+    print('If you wan to exit write "exit" in login.')
+    login = input("Insert Your login: ")
+    if login == 'exit': exit("Bye, have a great time!")
+    password = getpass("Insert Your password: ")
 
-temp = users.find_one({'login': f'{login}', 'password': f'{password}'})
+    temp = users.find_one({'login': f'{login}', 'password': f'{password}'})
 
-if temp == None:
-    print("Wrong login or password!")
-else:
-    user = users.find_one({'login': f'{login}', 'password': f'{password}'})
-    details = user['details']
-    print(f"Welcome back {details['fname']}!")
+    if temp == None:
+        print("Wrong login or password!")
+    else:
+        user = users.find_one({'login': f'{login}', 'password': f'{password}'})
+        details = user['details']
+        print(f"Welcome back {details['fname']}!")
