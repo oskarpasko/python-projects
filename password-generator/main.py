a8c2cb5788e5541c7a7d260cf4febd01077306d2
Oskar <oskarpasko@gmail.com>
2023-08-28 11:08:46 +0200

    add some styles

--- password-generator/main.py
+++ password-generator/main.py
@@ -1,22 +1,28 @@
 from password import Password
+from settings import Colors
 
 setting = []
 for x in range(0, 5):
     match(x):
         case 0:
-            setting.append(int(input("Input password's length: ")))
+            setting.append(int(input(f"{Colors.CYAN}Input password's length: ")))
+            print()
         case 1:
-            print(f"{setting[0]} characters left.")
-            setting.append(int(input("Input how many lower cases: ")))
+            print(f"{Colors.LIGHT_GREEN}{setting[0]} characters left.")
+            setting.append(int(input(f"{Colors.CYAN}Input how many lower cases: ")))
+            print()
         case 2:
-            print(f"{setting[0]-setting[1]} characters left.")
-            setting.append(int(input("Input how many upper cases: ")))
+            print(f"{Colors.LIGHT_GREEN}{setting[0]-setting[1]} characters left.")
+            setting.append(int(input(f"{Colors.CYAN}Input how many upper cases: ")))
+            print()
         case 3:
-            print(f"{setting[0]-setting[1]-setting[2]} characters left.")
-            setting.append(int(input("Input how many digits: ")))
+            print(f"{Colors.LIGHT_GREEN}{setting[0]-setting[1]-setting[2]} characters left.")
+            setting.append(int(input(f"{Colors.CYAN}Input how many digits: ")))
+            print()
         case 4:
-            print(f"{setting[0]-setting[1]-setting[2]-setting[3]} characters left.")
-            setting.append(int(input("Input how many special characters: ")))
+            print(f"{Colors.LIGHT_GREEN}{setting[0]-setting[1]-setting[2]-setting[3]} characters left.")
+            setting.append(int(input(f"{Colors.CYAN}Input how many special characters: {Colors.END}")))
+            print()
 
 test = Password(setting[0], setting[1], setting[2], setting[3], setting[4])
 
