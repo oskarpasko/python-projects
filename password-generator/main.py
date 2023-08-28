b9f607fd9b86a4ebbfc819c391a0ef8266eb908f
Oskar <oskarpasko@gmail.com>
2023-08-28 11:03:51 +0200

    Update main.py
    - added loop to create password's settings by user

--- password-generator/main.py
+++ password-generator/main.py
@@ -1,11 +1,23 @@
 from password import Password
 
-x = int(input("Podaj length"))
-y = int(input("Podaj lower"))
-z = int(input("Podaj upper"))
-a = int(input("Podaj digit"))
-b = int(input("Podaj special"))
+setting = []
+for x in range(0, 5):
+    match(x):
+        case 0:
+            setting.append(int(input("Input password's length: ")))
+        case 1:
+            print(f"{setting[0]} characters left.")
+            setting.append(int(input("Input how many lower cases: ")))
+        case 2:
+            print(f"{setting[0]-setting[1]} characters left.")
+            setting.append(int(input("Input how many upper cases: ")))
+        case 3:
+            print(f"{setting[0]-setting[1]-setting[2]} characters left.")
+            setting.append(int(input("Input how many digits: ")))
+        case 4:
+            print(f"{setting[0]-setting[1]-setting[2]-setting[3]} characters left.")
+            setting.append(int(input("Input how many special characters: ")))
 
-test = Password(x, y, z, a, b)
+test = Password(setting[0], setting[1], setting[2], setting[3], setting[4])
 
 test.create_password()
